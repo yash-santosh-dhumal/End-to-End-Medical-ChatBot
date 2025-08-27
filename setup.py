1e94fc5ebb3dc7347298537bd7f40d46da29ad9f
yashdhumal <yashdhumal90@gmail.com>
2025-08-27 16:22:56 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="End-to-End-Medibot",
+    version='0.0.0',
+    author = 'Yash Dhumal',
+    author_email='yashdhumal90@gmail.com',
+    packages=find_packages(),
+    install_reuires = []
+)
